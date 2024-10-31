f3a3d4ebdfd42f4639e726abebaef08423e76fb0
Psage01 <markus01.erasmus@gmail.com>
2024-10-31 16:38:48 +0200

    Styling

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:expenses_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 255, 255, 255),
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
+        useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
@@ -29,9 +30,9 @@ void main() {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
+                fontWeight: FontWeight.bold,
                 color: kColorScheme.onSecondaryContainer,
-                fontSize: 14,
+                fontSize: 16,
               ),
             ),
       ),
